ee6d992699aacb3664ae6584d8a624315bfd74a8
Jez Humble <jez@jezhumble.net>
2013-03-18 23:31:21 -0500

    Refactored weak checksum algorithm

--- src/rsync/checksum.clj
+++ src/rsync/checksum.clj
@@ -4,29 +4,18 @@
 (def md5-digest (MessageDigest/getInstance "MD5"))
 
 (defn sum-bytes [bytes index length]
-  (loop [total 0
+  (loop [sum 0
+         sum-product 0
          i index]
     (if (< i length)
-      (recur (+ total (bit-and (aget bytes i) 0xff))
+      (recur (+ sum (bit-and (aget bytes i) 0xff))
+             (+ sum-product (* (bit-and (aget bytes i) 0xff) (- length i)))
              (inc i))
-      total)))
-
-(defn sum-product-bytes [bytes index length]
-  (loop [total 0
-         i index]
-    (if (< i length)
-      (recur (+ total (* (bit-and (aget bytes i) 0xff) (- length i)))
-             (inc i))
-      total)))
-           
-(defn weak-checksum-a [bytes length modulo]
-  (rem (sum-bytes bytes 0 length) modulo))
-
-(defn weak-checksum-b [bytes length modulo]
-  (rem (sum-product-bytes bytes 0 length) modulo))
+      [sum, sum-product])))
 
 (defn weak-checksum [bytes length modulo]
-  (+ (weak-checksum-a bytes length modulo) (* modulo (weak-checksum-b bytes length modulo))))
+  (let [[sum, sum-product] (sum-bytes bytes 0 length)]
+    (+ (rem sum modulo) (* modulo (rem sum-product modulo)))))
 
 (defn md5 [bytes offset block-size]
   (do
